e54e0409b232bab7af7c851a4b18bc35d2674425
shouston3 <shouston3@me.com>
2016-05-18 09:19:08 +0100

    ui works with new generations every .4 seconds

--- helpers/index.js
+++ helpers/index.js
@@ -30,7 +30,62 @@ const generateGridFromCoords = (arr, size) => {
   });
 }
 
+const returnAllCellsToConsiderFromLiveCells = grid => {
+  return grid.reduce((prev, curr) => {
+    const x = parseInt(curr.split(',')[0]);
+    const y = parseInt(curr.split(',')[1]);
+    const arr = [-1, 0, 1].reduce((previ, curri) => {
+      return previ.concat([-1, 0, 1].reduce((prevj, currj) => prevj.concat((x + currj) + ',' + (y + curri)), []));
+    }, []);
+    return prev.concat(arr)
+  }, []);
+}
+
+const removeDuplicates = arr => arr.filter((el, i) => arr.indexOf(el) === i);
+
+const cellsToConsider = grid => {
+  return removeDuplicates(returnAllCellsToConsiderFromLiveCells(grid));
+}
+
+const mapLiveCellsToCellsToConsider = (liveCells, cellsToConsider) => {
+  return cellsToConsider.map(el => {
+    return {coord: el, state: liveCells.indexOf(el) > -1 ? 1 : 0}
+  });
+}
+
+const cellsToConsiderWithState = grid => {
+  return mapLiveCellsToCellsToConsider(grid, cellsToConsider(grid))
+}
+
+const getNeighboursOfObjs = arr => arr.map(el => {
+  const f = el.coord.split(',')[0];
+  const s = el.coord.split(',')[1];
+  const neighbours = arr.filter(e => {
+    return Math.abs(e.coord.split(',')[0] - f) < 2 && Math.abs(e.coord.split(',')[1] - s) < 2
+  }).reduce((prev, curr) => prev + curr.state, 0) - el.state;
+  const obj = {};
+  obj.coord = el.coord;
+  obj.state = el.state;
+  obj.neighbours = neighbours;
+  return obj
+});
+
+const getNeighbours = grid => getNeighboursOfObjs(cellsToConsiderWithState(grid));
+
+const newGeneration = grid => {
+  return getNeighbours(grid).map(el => {
+    const obj = {};
+    obj.coord = el.coord;
+    obj.state = el.neighbours < 2 || el.neighbours > 3 ? 0 : el.neighbours === 3 ? 1 : el.state
+    return obj
+  }).filter(el => el.state).map(el => el.coord);
+}
+
 module.exports = {
   generateCoordsFromGrid,
-  generateGridFromCoords
+  generateGridFromCoords,
+  cellsToConsider,
+  cellsToConsiderWithState,
+  getNeighbours,
+  newGeneration
 }
